defd9df1da2e237638d51ee6d65a8d5f8f66868a
robsen123-fexil <robsenteshoma1@gmail.com>
2024-03-19 11:39:01 +0300

    web flask

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""project """
+""" Amenity Module for HBNB project """
 import os
-from sqlalchemy import  String , Column
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-from models.base_model import  Base , BaseModel
+from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
-    """Amenity data """
+    """Represents an amenity data set."""
     __tablename__ = 'amenities'
     name = Column(
         String(128), nullable=False
